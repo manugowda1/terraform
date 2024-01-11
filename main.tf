35f84cda3ddee94b2b0b7331af69918de041e27a
manu <manuazure13@gmail.com>
2024-01-11 11:30:56 +0530

    rg

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
 # Configure Azure Provider
 provider "azurerm" {
   # Version is optional
-  # Terraform recommends to pin to a specific version of provider
+  # Terraform recommends to pin to a specific version of provide
   #version = "=2.35.0"
   #version = "~>2.35.0"
-  #version = "~> 2.37.0"
+  
   features {}
+}
+
+ 
+# Create a Azure Resource Group
+resource "azurerm_resource_group" "aks-rg2" {
+  name     = "aks-rg2-tf"
+  location = "East US"
+
+
+# Add Tags
+#  tags = {
+#    "environment" = "k8sdev"
+#    "demotag"     = "refreshtest"
+#  }
+
 }
